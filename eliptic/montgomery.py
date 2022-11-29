30f0716242cc2ad0b437c5cb01fa68b66de3c358
SnickeyX <s2152674@ed.ac.uk>
2022-11-29 19:21:15 +0000

    montgomery init

---
+++ eliptic/montgomery.py
@@ -0,0 +1,5 @@
+# for the montgomery curve given as By^2 = x^3 + Ax^2 + x over some field K
+# TODO: implement addition and doubling 
+
+# alternatively you can convert the curve to Weierstrass -> https://en.wikipedia.org/wiki/Montgomery_curve#:~:text=.-,Equivalence%20with%20Weierstrass%20curves,-%5Bedit%5D
+# and then use the Weierstrass formulas for addition and doubling
